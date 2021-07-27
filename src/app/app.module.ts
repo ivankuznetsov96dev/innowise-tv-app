804c6d9e1255a29048ef94d75d8ed6666ba590db
Ivan Kuzniatsou <ivan.kuzniatsou@innowise-group.com>
2021-07-27 15:04:21 +0300

    feat: imports ngrx store, store devtools and effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -17,6 +20,7 @@ import { ChannelsModule } from './modules/channels/channels.module';
 import { ChannelInfoModule } from './modules/channel-info/channel-info.module';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { AuthInterceptor } from './services/auth-interceptoe.service';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, LoginFormComponent],
@@ -36,6 +40,13 @@ import { AuthInterceptor } from './services/auth-interceptoe.service';
     MatSnackBarModule,
     MatSlideToggleModule,
     RxReactiveFormsModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
   ],
   providers: [
     {
